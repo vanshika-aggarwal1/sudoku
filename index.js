41f0ff0ae084e914a0a104231a640aba5c645b51
Vanshika Aggarwal <141432105+vanshika-aggarwal1@users.noreply.github.com>
2026-07-05 16:59:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import axios from "axios";
 import dotenv from "dotenv";
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 3000;
 const apikey = process.env.MY_API_KEY;
 
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
