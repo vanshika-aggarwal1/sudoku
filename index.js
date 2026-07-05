e7e78b88d4cd5a69fac16741b0468da2cee54d1d
Vanshika Aggarwal <141432105+vanshika-aggarwal1@users.noreply.github.com>
2026-07-05 17:06:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const apikey = process.env.MY_API_KEY;
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 app.get("/", async (req, res) => {
